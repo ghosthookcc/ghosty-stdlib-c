7fc8730333f6d6ee4fce27cc9d9ab520f621168e
Kasper Anon <kasperthornsten@gmail.com>
2024-05-29 18:36:39 +0200

    Implement vector data structure

--- Header Files/stdlib/vector.h
+++ Header Files/stdlib/vector.h
@@ -1,4 +1,35 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+typedef struct fvector3_t
+{
+	float x;
+	float y;
+	float z;
+} fvector3;
+
+typedef struct fvector2_t
+{
+	float x;
+	float y;
+} fvector2;
+
+typedef struct dvector3_t
+{
+	double x;
+	double y;
+	double z;
+} dvector3;
+
+typedef struct dvector2_t
+{
+	double x;
+	double y;
+} dvector2;
+
+fvector3 fvec3(float x, float y, float z);
+fvector2 fvec2(float x, float y);
+dvector3 dvec3(double x, double y, double z);
+dvector2 dvec2(double x, double y);
+
 #endif
--- Source Files/stdlib/vector.c
+++ Source Files/stdlib/vector.c
@@ -1,1 +1,35 @@
 #include "../../Header Files/stdlib/vector.h"
+
+fvector3 fvec3(float x, float y, float z)
+{
+	fvector3 newVector3;
+	newVector3.x = x;
+	newVector3.y = y;
+	newVector3.z = z;
+	return newVector3;
+}
+
+fvector2 fvec2(float x, float y)
+{
+	fvector2 newVector2;
+	newVector2.x = x;
+	newVector2.y = y;
+	return newVector2;
+}
+
+dvector3 dvec3(double x, double y, double z)
+{
+	dvector3 newdVector3;
+	newdVector3.x = x;
+	newdVector3.y = y;
+	newdVector3.z = z;
+	return newdVector3;
+}
+
+dvector2 dvec2(double x, double y)
+{
+	dvector2 newdVector2;
+	newdVector2.x = x;
+	newdVector2.y = y;
+	return newdVector2;
+}
