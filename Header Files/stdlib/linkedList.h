e15b51af4315238ebd4166ed00e3d8aace6d9b53
ghosthookcc <kasperthornsten@gmail.com>
2024-08-15 08:39:31 +0200

    Partly Implement linkedList datastructure

--- Header Files/stdlib/linkedList.h
+++ Header Files/stdlib/linkedList.h
@@ -1,4 +1,29 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct node_t
+{
+    void* value;
+    struct node_t* next;
+    struct node_t* previous;
+    // Why not previous too?
+} *node;
+
+typedef struct linkedList_t
+{
+    int size;
+    node head;
+    // Why not tail?
+} linkedList;
+
+node initLinkedListNode(void* inData);
+
+linkedList initLinkedList();
+void insertIntoLinkedListBeginning(linkedList* targetPtr,  node inData);
+void insertIntoLinkedListEnd(linkedList* targetPtr, node inData);
+
 #endif // LINKEDLIST_H
--- Source Files/stdlib/linkedList.c
+++ Source Files/stdlib/linkedList.c
@@ -1,1 +1,34 @@
 #include "../../Header Files/stdlib/linkedList.h"
+
+node initLinkedListNode(void* inData)
+{
+    node newNode = (node)malloc(sizeof(*newNode) + sizeof(inData));
+    newNode->value = inData;
+    newNode->next = NULL;
+    newNode->previous = NULL;
+    return newNode;
+}
+
+linkedList initLinkedList()
+{
+    linkedList newLinkedList;
+    newLinkedList.size = 0;
+    newLinkedList.head = NULL;
+    return newLinkedList;
+}
+
+void insertIntoLinkedListBeginning(linkedList* targetPtr, node newNode)
+{
+    newNode->next = targetPtr->head;
+    if (targetPtr->head != NULL)
+    {
+        targetPtr->head->previous = newNode;
+    }
+    targetPtr->head = newNode;
+    targetPtr->size += 1;
+}
+
+void insertIntoLinkedListEnd(linkedList* targetPtr, node newNode)
+{
+
+}
