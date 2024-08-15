f1194bcfb2b42c37f91f37d019b6b3192ff4f395
ghosthookcc <kasperthornsten@gmail.com>
2024-08-15 13:35:34 +0200

    Implement basic hashtable bucket structure and template for future hashtable implementation

--- Header Files/stdlib/hashtable.h
+++ Header Files/stdlib/hashtable.h
@@ -1,4 +1,25 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include "hash.h"
+#include "linkedList.h"
+
+typedef struct hashTableBucket_t
+{
+    bucket bucket;
+    linkedList data;
+} hashTableBucket;
+
+// Speed linear with load factor
+typedef struct chainedHashTable_t
+{
+
+} *chainedHashTable;
+
+// Speed under low load factor
+typedef struct addressedHashTable_t
+{
+
+} *openHashTable;
+
 #endif // HASHTABLE_H
