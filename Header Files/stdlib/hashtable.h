06a0dbff8add6bbebc69acd5064cd05a5dc09ecb
ghosthookcc <kasperthornsten@gmail.com>
2024-09-03 14:25:21 +0200

    Implement hashtable data structure

--- Header Files/stdlib/hashtable.h
+++ Header Files/stdlib/hashtable.h
@@ -6,14 +6,24 @@
 
 typedef struct hashTableBucket_t
 {
-    bucket bucket;
-    linkedList data;
-} hashTableBucket;
+    bucket _bucket;
+    void* data;
+} *hashTableBucket;
+
+typedef struct keyPair_t
+{
+    void* key;
+    void* value;
+    unsigned int keyLength;
+} *keyPair;
 
 // Speed linear with load factor
 typedef struct chainedHashTable_t
 {
-
+    //hashTableBucket* buckets;
+    linkedList* chains;
+    unsigned int entryCount;
+    unsigned int capacity;
 } *chainedHashTable;
 
 // Speed under low load factor
@@ -21,5 +31,12 @@ typedef struct addressedHashTable_t
 {
 
 } *openHashTable;
+
+chainedHashTable initChainedHashTable(unsigned int capacity);
+void insertIntoChainedHashTable(chainedHashTable* targetPtr, keyPair keyPair);
+keyPair searchChainedHashTable(chainedHashTable targetPtr, keyPair keyPair);
+
+keyPair initKeyPair(void* key, void* value, unsigned int keyLength);
+hashTableBucket initHashTableBucket(unsigned int hash, void* data);
 
 #endif // HASHTABLE_H
--- Source Files/stdlib/hashtable.c
+++ Source Files/stdlib/hashtable.c
@@ -1,1 +1,98 @@
 #include "../../Header Files/stdlib/hashtable.h"
+
+chainedHashTable initChainedHashTable(unsigned int capacity)
+{
+    chainedHashTable newHashTable = (chainedHashTable)malloc(sizeof(*newHashTable) + sizeof(linkedList) * capacity);
+    newHashTable->chains = (linkedList*)malloc(sizeof(linkedList) * capacity);
+    newHashTable->entryCount = 0;
+    newHashTable->capacity = capacity;
+    return newHashTable;
+}
+
+void insertIntoChainedHashTable(chainedHashTable* targetPtr, keyPair keyPairToInsert)
+{
+    chainedHashTable dtarget = *targetPtr;
+    unsigned int hash = fnv1AHash(keyPairToInsert->key, keyPairToInsert->keyLength, 0);
+    unsigned int hashTableIdx = hash % dtarget->capacity;
+
+    if (dtarget->chains[hashTableIdx] == NULL)
+    {
+        dtarget->chains[hashTableIdx] = initLinkedList();
+    }
+
+    linkedList entry = dtarget->chains[hashTableIdx];
+    node newEntryNode = NULL;
+    if (entry->head == NULL)
+    {
+        hashTableBucket newBucket = initHashTableBucket(hash, keyPairToInsert);
+        newBucket->_bucket->size = 1;
+        newEntryNode = initLinkedListNode(newBucket);
+    }
+    else
+    {
+        node currentNode = entry->head;
+        hashTableBucket currentBucket = NULL;
+        while (currentNode != NULL)
+        {
+            currentBucket = ((hashTableBucket)currentNode->value);
+            if (currentBucket->_bucket == NULL)
+            {
+                hashTableBucket newBucket = initHashTableBucket(hash, keyPairToInsert);
+                newEntryNode = initLinkedListNode(&newBucket);
+                currentNode->value = newEntryNode;
+                newBucket->_bucket->size += 1;
+                currentBucket = newBucket;
+            }
+            else if (currentBucket->_bucket->hash == hash)
+            {
+                free(currentBucket->data);
+                currentBucket->data = keyPairToInsert;
+            }
+            currentNode = currentNode->next;
+        }
+    }
+
+    if (newEntryNode != NULL)
+    {
+        insertIntoLinkedListBeginning(&entry, newEntryNode);
+        dtarget->entryCount += 1;
+    }
+}
+
+keyPair searchChainedHashTable(chainedHashTable targetPtr, keyPair keyPair)
+{
+    unsigned int hash = fnv1AHash(keyPair->key, keyPair->keyLength, 0);
+    unsigned int hashTableIdx = hash % targetPtr->capacity;
+
+    linkedList entry = targetPtr->chains[hashTableIdx];
+
+    node currentNode = entry->head;
+    hashTableBucket currentBucket = NULL;
+    while (currentNode != NULL)
+    {
+        currentBucket = ((hashTableBucket)currentNode->value);
+        if (currentBucket != NULL && currentBucket->_bucket->hash == hash)
+        {
+            return (currentBucket->data);
+        }
+        currentNode = currentNode->next;
+    }
+    return NULL;
+}
+
+keyPair initKeyPair(void* key, void* value, unsigned int keyLength)
+{
+    keyPair newKeyPair = (keyPair)malloc(sizeof(*newKeyPair) + keyLength + sizeof(value));
+    newKeyPair->key = key;
+    newKeyPair->value = value;
+    newKeyPair->keyLength = keyLength;
+    return newKeyPair;
+}
+
+hashTableBucket initHashTableBucket(unsigned int hash, void* data)
+{
+    hashTableBucket newHashTableBucket = (hashTableBucket)malloc(sizeof(*newHashTableBucket));
+    newHashTableBucket->_bucket = initHashBucket(hash);
+    newHashTableBucket->data = data;
+    return newHashTableBucket;
+}
