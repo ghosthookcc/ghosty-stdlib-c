86c1d249c01325731414a375b94b3685282f4278
ghosthookcc <kasperthornsten@gmail.com>
2024-09-22 16:56:10 +0200

    Added includes for all new functionality headers

--- Header Files/stdlib/mystdlib.h
+++ Header Files/stdlib/mystdlib.h
@@ -1,15 +1,20 @@
 #ifndef MY_STDLIB_H
 #define MY_STDLIB_H
 
-#include "string.h"
+#include "boolean.h"
+#include "common.h"
 #include "dArray.h"
 #include "file.h"
-#include "math.h"
-#include "matrix.h"
-#include "vector.h"
+#include "hash.h"
 #include "hashtable.h"
 #include "linkedList.h"
+#include "math.h"
+#include "matrix.h"
+#include "quaternion.h"
 #include "queue.h"
 #include "stack.h"
+#include "string.h"
+#include "timetest.h"
+#include "vector.h"
 
 #endif // MY_STDLIB_H
