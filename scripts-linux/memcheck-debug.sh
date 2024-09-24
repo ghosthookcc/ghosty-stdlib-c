d24e1dbcab4bfdf88c81b32a819431cbc30d06b4
ghosthookcc <kasperthornsten@gmail.com>
2024-09-25 00:21:57 +0200

    Add -s argument for valgrind to show all error types

--- scripts-linux/memcheck-debug.sh
+++ scripts-linux/memcheck-debug.sh
@@ -4,7 +4,7 @@ echo
 echo "[/][ANY] to run valgrind for debug executable . . ."
 export DEBUGINFOD_URLS=https://debuginfod.archlinux.org 
 read
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./../Debug/bin/Debug
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./../Debug/bin/Debug
 echo
 echo "[/][ANY] to exit memcheck-debug script . . ."
 read
--- scripts-linux/memcheck-release.sh
+++ scripts-linux/memcheck-release.sh
@@ -4,7 +4,7 @@ echo
 echo "[/][ANY] to run valgrind for release executable . . ."
 export DEBUGINFOD_URLS=https://debuginfod.archlinux.org 
 read
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./../Release/bin/Release
+valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./../Release/bin/Release
 echo
 echo "[/][ANY] to exit memcheck-release script . . ."
 read
