0dc0168c0a8b33e1a670ee20dc8572aeab583bb2
ghosthookcc <kasperthornsten@gmail.com>
2024-09-25 00:23:56 +0200

    Implement debug print function when debug flag is set common.h

--- Header Files/stdlib/common.h
+++ Header Files/stdlib/common.h
@@ -1,6 +1,15 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <stdio.h>
+#include <stdarg.h>
+
+#if defined(DEBUG_MODE_ON) && DEBUG_MODE_ON
+#define DebugPrint(format, ...) printf(format, ##__VA_ARGS__)
+#else 
+#define DebugPrint(format, ...)
+#endif
+
 #define STATIC_CAST(type, expr)  ((type)(expr))
 #define VOIDPTR_CAST(type, expr) (*(type*)(expr))
 
