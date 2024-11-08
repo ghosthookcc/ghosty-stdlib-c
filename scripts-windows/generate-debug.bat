058e0ce445701879874d77507bc62a2b2680b5d1
Kasper Anon <kasperthornsten@gmail.com>
2024-11-08 17:04:59 +0100

    Add bit.h + more to standard library build for windows

--- scripts-windows/generate-debug.bat
+++ scripts-windows/generate-debug.bat
@@ -3,7 +3,7 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 echo.
 echo [+] Call to open visual studio developer cmd complete . . .
 echo.
-cl ../main.c "../Source Files/stdlib/dArray.c" "../Source Files/stdlib/file.c" "../Source Files/stdlib/string.c" "../Source Files/stdlib/math.c" "../Source Files/stdlib/matrix.c" "../Source Files/stdlib/hash.c" "../Source Files/stdlib/hashtable.c" "../Source Files/stdlib/linkedList.c" "../Source Files/stdlib/queue.c" "../Source Files/stdlib/stack.c" "../Source Files/stdlib/vector.c" /fsanitize=address /Zi /Od /Fe:../Debug/bin/Debug.exe /Fo:../Debug/obj/ /Fd:../Debug/pdb/Debug.pdb
+cl ../main.c "../Source Files/stdlib/bit.c" "../Source Files/stdlib/boolean.c" "../Source Files/stdlib/common.c" "../Source Files/stdlib/dArray.c" "../Source Files/stdlib/file.c" "../Source Files/stdlib/math.c" "../Source Files/stdlib/matrix.c" "../Source Files/stdlib/memory.c" "../Source Files/stdlib/hash.c" "../Source Files/stdlib/hashtable.c" "../Source Files/stdlib/linkedList.c" "../Source Files/stdlib/queue.c" "../Source Files/stdlib/stack.c" "../Source Files/stdlib/string.c" "../Source Files/stdlib/timetest.c" "../Source Files/stdlib/vector.c" /fsanitize=address /Zi /Od /Fe:../Debug/bin/Debug.exe /Fo:../Debug/obj/ /Fd:../Debug/pdb/Debug.pdb
 echo.
 echo [+] Built project . . .
 echo.
