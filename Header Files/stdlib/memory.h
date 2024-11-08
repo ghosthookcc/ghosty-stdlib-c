dfed908b63d355887eb8a37183d1a187ce6234c7
Kasper Anon <kasperthornsten@gmail.com>
2024-11-08 01:02:28 +0100

    Implement working memory arena struct

--- Header Files/stdlib/memory.h
+++ Header Files/stdlib/memory.h
@@ -6,23 +6,29 @@
 #include <string.h>
 #include <stdio.h>
 
+#define ARENAENTRYTAKEN ((unsigned int)~0)
+
 typedef struct arena_t
 {
 	size_t storageSizeInBytes;
 	size_t realStorageSizeInBytes;
-	void* startOffsetInMemory;
-	void* lastInsertInMemory;
-	void* endOffsetInMemory;
-	unsigned char* storage;
 	struct arena_t* parent;
-} arena;
+    unsigned char* startOffsetInMemory;
+	unsigned char* endOffsetInMemory;
+	unsigned char* lastInsertInMemory;
+	unsigned char* storage;
+} *arena;
 
-arena initArena(size_t storageSizeInBytes);
-arena initArenaFromArena(arena* other, size_t storageSizeInBytes);
+arena initArena(size_t storageSizeInBytes, size_t alignment);
+arena initArenaFromArena(arena other, size_t storageSizeInBytes);
 
-void insertIntoArena(arena targetPtr, void* data, size_t dataLength);
+void insertIntoArena(arena targetPtr, void* data, size_t dataLength, unsigned int idx);
+
 void getArenaAtIndex(arena targetPtr, void* out, unsigned int idx, size_t size);
+void* getArenaMemoryAtIndex(arena targetPtr, unsigned int idx, size_t size);
 
-void freeArena(arena* targetPtr);
+void freeArena(arena targetPtr);
+
+void gsmemset(void* destination, size_t setSize, int setTo);
 
 #endif // MEMORY_H
--- Source Files/stdlib/memory.c
+++ Source Files/stdlib/memory.c
@@ -1,40 +1,47 @@
 #include "../../Header Files/stdlib/memory.h"
 
-arena initArena(size_t storageSizeInBytes)
+arena initArena(size_t storageSizeInBytes, size_t alignment)
 {
-	arena newArena;
-	newArena.storageSizeInBytes = storageSizeInBytes;
-	newArena.realStorageSizeInBytes = 0;
+	arena newArena = malloc(sizeof(*newArena));
+	newArena->storageSizeInBytes = storageSizeInBytes;
+	newArena->realStorageSizeInBytes = 0;
 
-	newArena.storage = aligned_alloc(1024, storageSizeInBytes);
-	memset(newArena.storage, ~((unsigned int)0), storageSizeInBytes);
+	newArena->storage = _aligned_malloc(storageSizeInBytes, alignment);
 
-	newArena.startOffsetInMemory = newArena.storage;
-	newArena.lastInsertInMemory = newArena.startOffsetInMemory;
-	newArena.endOffsetInMemory = newArena.storage + storageSizeInBytes;
+    if (newArena->storage == NULL || ((uintptr_t)newArena->storage % alignment) != 0) 
+    {
+        printf("Error: Memory allocation failed or is misaligned.\n");
+        // Handle the error, possibly by freeing and returning NULL
+    }
 
-	newArena.parent = NULL;
+	memset(newArena->storage, ARENAENTRYTAKEN, storageSizeInBytes);
+
+	newArena->startOffsetInMemory = newArena->storage;
+	newArena->endOffsetInMemory = newArena->storage + storageSizeInBytes;
+	newArena->lastInsertInMemory = newArena->storage;
+
+	newArena->parent = NULL;
 	
 	return newArena;
 }
 
-arena initArenaFromArena(arena* other, size_t storageSizeInBytes)
+arena initArenaFromArena(arena other, size_t storageSizeInBytes)
 {
-	if (storageSizeInBytes >= other->storageSizeInBytes) return *other;
+	if (storageSizeInBytes >= other->storageSizeInBytes) return other;
 
-	arena newArena;
-	newArena.storageSizeInBytes = storageSizeInBytes;
-	newArena.realStorageSizeInBytes = 0;
+	arena newArena = malloc(sizeof(*newArena));
+	newArena->storageSizeInBytes = storageSizeInBytes;
+	newArena->realStorageSizeInBytes = 0;
 	
-	void* otherArenaNewMemoryEndOffset = &other->endOffsetInMemory - storageSizeInBytes;
+	unsigned char* otherArenaNewMemoryEndOffset = other->endOffsetInMemory - storageSizeInBytes;
 	
-	newArena.startOffsetInMemory = otherArenaNewMemoryEndOffset;
-	newArena.lastInsertInMemory = newArena.startOffsetInMemory;
-	newArena.endOffsetInMemory = &otherArenaNewMemoryEndOffset + storageSizeInBytes;
+	newArena->startOffsetInMemory = otherArenaNewMemoryEndOffset;
+	newArena->lastInsertInMemory = newArena->startOffsetInMemory;
+	newArena->endOffsetInMemory = otherArenaNewMemoryEndOffset + storageSizeInBytes;
 
-	newArena.storage = otherArenaNewMemoryEndOffset;
+	newArena->storage = otherArenaNewMemoryEndOffset;
 	
-	newArena.parent = other;
+	newArena->parent = other;
 
 	other->storageSizeInBytes -= storageSizeInBytes;
 	other->endOffsetInMemory = otherArenaNewMemoryEndOffset;
@@ -42,24 +49,47 @@ arena initArenaFromArena(arena* other, size_t storageSizeInBytes)
 	return newArena;
 }
 
-void insertIntoArena(arena targetPtr, void* data, size_t dataLength)
+void insertIntoArena(arena targetPtr, void* data, size_t dataLength, unsigned int idx)
 {
-	if (targetPtr.realStorageSizeInBytes + dataLength > targetPtr.storageSizeInBytes) return;
-	printf("\n\n%p\n\n", data);
-	memcpy(targetPtr.lastInsertInMemory, data, dataLength);
-	targetPtr.lastInsertInMemory = (unsigned char*)targetPtr.lastInsertInMemory+dataLength;
+	if (targetPtr->realStorageSizeInBytes + dataLength > targetPtr->storageSizeInBytes) return;	
+
+    if (idx != ARENAENTRYTAKEN)
+    {
+        unsigned char* insertAtMemoryAddress = targetPtr->storage+(idx * dataLength);
+        memcpy(insertAtMemoryAddress, data, dataLength);
+    }
+    else 
+    {
+        memcpy(targetPtr->lastInsertInMemory, data, dataLength);
+        targetPtr->lastInsertInMemory += dataLength;
+    }
+    targetPtr->realStorageSizeInBytes += dataLength; 
 }
 
 void getArenaAtIndex(arena targetPtr, void* out, unsigned int idx, size_t size)
 {
-	memcpy(out, (unsigned char*)targetPtr.storage+(idx * size), size);
+	memcpy(out, targetPtr->storage+(idx * size), size);
 }
 
-void freeArena(arena* targetPtr)
+void* getArenaMemoryAtIndex(arena targetPtr, unsigned int idx, size_t size)
 {
-	arena dtarget = *targetPtr;
-	if (dtarget.parent == NULL) 
-	{ 
-		free(dtarget.storage); 
-	}
+    return targetPtr->storage+(idx * size);
+}
+
+void freeArena(arena targetPtr)
+{
+    //if (targetPtr->parent == NULL)
+    //{
+    //    _aligned_free(targetPtr->storage); 
+    //}
+    free(targetPtr);
+}
+
+void gsmemset(void* destination, size_t setSize, int setTo)
+{
+    unsigned char* memoryStartAddressPtr = destination;
+    for (size_t idx = 0; idx < setSize; idx++)
+    {
+       *memoryStartAddressPtr++ = setTo;
+    }
 }
